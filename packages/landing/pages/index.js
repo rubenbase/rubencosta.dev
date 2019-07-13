d22cfce06228b3a34309a89308e5c292c400bb34
Ruben Costa <daggosso2@gmail.com>
2019-07-13 13:22:56 +0200

    TEST: Testing Continous Deployment in master branch

--- packages/landing/pages/index.js
+++ packages/landing/pages/index.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 export default () => {
   return (
     <div>
-      <h3>Personal website</h3>
+      <h3>Personal website with Continous Deployment</h3>
     </div>
   );
 };
