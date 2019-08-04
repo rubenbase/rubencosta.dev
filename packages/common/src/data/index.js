2c99f63772f906a0b93cfb3866979b721336f4c2
Ruben Costa <daggosso2@gmail.com>
2019-08-04 23:27:29 +0200

    ADD: Data folder

---
+++ packages/common/src/data/index.js
@@ -0,0 +1,12 @@
+export const MENU_ITEMS = [
+    {
+        label: "ME",
+        path: "#banner_section",
+        offset: "0"
+    },
+    {
+        label: "PROJECTS",
+        path: "#portfolio_section",
+        offset: "0"
+    }
+];
