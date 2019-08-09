ff6a9602b84e2b871ca4435b34696b688f51f65e
Ruben Costa <daggosso2@gmail.com>
2019-08-09 15:50:32 +0200

    ADD: Finish Header of portfolio section

--- packages/landing/containers/PortfolioShowcase/index.js
+++ packages/landing/containers/PortfolioShowcase/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
+
 import Box from "reusecore/src/elements/box";
+import Heading from "reusecore/src/elements/Heading";
+import Text from "reusecore/src/elements/Text";
 
 import Container from "common/src/components/ui/container";
 
-function PortfolioShowcase({ sectionWrapper, secTitleWrapper, secTitle }) {
+function PortfolioShowcase({
+    sectionWrapper,
+    secTitleWrapper,
+    secTitle,
+    secDescription
+}) {
     return (
         <Box {...sectionWrapper} as="section" id="portfolio_section">
             <Container noGutter mobileGutter width="1200px">
@@ -13,7 +21,11 @@ function PortfolioShowcase({ sectionWrapper, secTitleWrapper, secTitle }) {
                         {...secTitle}
                         content="Making Ideas Come to Life !"
                     />
-                    <h4>This is an example title</h4>
+                    <Text
+                        {...secDescription}
+                        content="This is a test, Ruben Costa has been created 
+                    to show the projects I made in my free time. Take a look!"
+                    />
                 </Box>
             </Container>
         </Box>
@@ -23,7 +35,8 @@ function PortfolioShowcase({ sectionWrapper, secTitleWrapper, secTitle }) {
 PortfolioShowcase.propTypes = {
     sectionWrapper: PropTypes.object,
     secTitleWrapper: PropTypes.object,
-    secTitle: PropTypes.object
+    secTitle: PropTypes.object,
+    secDescription: PropTypes.object
 };
 
 PortfolioShowcase.defaultProps = {
@@ -41,6 +54,13 @@ PortfolioShowcase.defaultProps = {
         color: "#302b4e",
         lineHeight: "1.34",
         mb: ["15px", "18px", "18px", "20px", "20px"]
+    },
+    secDescription: {
+        fontSize: ["15px", "16px"],
+        fontWeight: "400",
+        color: "#43414e",
+        lineHeight: "1.5",
+        mb: "0"
     }
 };
 
