d25fffb49c3847834feb27b275360809bf87d392
Ruben Costa <costamartinezruben@gmail.com>
2020-03-31 23:57:55 +0100

    change color name

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,9 +1,8 @@
 export default {
 	primary: '#21ce99',
 	secondary: 'blue',
-	grey1: '#f9f9f9',
-	grey1: 'rgb(247, 248, 252)',
 	white: '#ffffff',
+	light1: 'rgb(247, 248, 252)',
 	black: '#000000',
 	dark: 'rgb(4, 13, 20)'
 }
