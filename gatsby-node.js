64e309b6cc5b22b09622fd5c02472b0ff35d931c
Ruben Costa <costamartinezruben@gmail.com>
2020-04-02 00:04:21 +0100

    link post url with component and content

--- gatsby-node.js
+++ gatsby-node.js
@@ -64,6 +64,44 @@
 // }
 
 const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require(`path`)
+
+exports.createPages = ({ actions, graphql }) => {
+	const { createPage } = actions
+	const blogPostTemplate = path.resolve('src/components/templates/blog-post/BlogPostTemplate.js')
+
+	return graphql(`
+		{
+			allMdx {
+				nodes {
+					fields {
+						slug
+					}
+					frontmatter {
+						title
+					}
+				}
+			}
+		}
+	`).then(result => {
+		if (result.errors) {
+			throw result.errors
+		}
+
+		const posts = result.data.allMdx.nodes
+
+		// create page for each mdx file
+		posts.forEach(post => {
+			createPage({
+				path: `blog${post.fields.slug}`,
+				component: blogPostTemplate,
+				context: {
+					slug: post.fields.slug
+				}
+			})
+		})
+	})
+}
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
 	const { createNodeField } = actions
