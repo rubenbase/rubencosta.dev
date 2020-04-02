12821364da8954ce2eba522bbf63b4ba62701d7d
Ruben Costa <costamartinezruben@gmail.com>
2020-04-02 19:20:30 +0100

    update root wrapper

--- src/utils/root-wrapper.js
+++ src/utils/root-wrapper.js
@@ -1,9 +1,36 @@
 import { MDXProvider } from '@mdx-js/react'
 import React from 'react'
+import Code from 'components/organisms/code'
+import { css, jsx } from '@emotion/core'
+import { colors } from 'styles'
 
 const components = {
-	h2: ({ children }) => <h2 style={{ color: 'rebeccapurple' }}>{children}</h2>,
-	'p.inlineCode': props => <code style={{ backgroundColor: 'lightgray' }} {...props} />
+	// h2: ({ children }) => <h2 style={{ color: 'rebeccapurple' }}>{children}</h2>,
+	// a: ({ children }) => <a style={{ color: colors.primary }}>{children}</a>,
+	a: props => (
+		<a {...props} style={{ color: '#19a764' }}>
+			{props.children}
+		</a>
+	),
+	inlineCode: props => (
+		<code
+			css={css`
+				background-color: rgb(36, 206, 153, 0.27);
+			`}
+			{...props}
+		/>
+	),
+	pre: ({ children: { props } }) => {
+		if (props.mdxType === 'code') {
+			return (
+				<Code
+					codeString={props.children.trim()}
+					language={props.className && props.className.replace('language-', '')}
+					{...props}
+				/>
+			)
+		}
+	}
 }
 
 export const wrapRootElement = ({ element }) => (
