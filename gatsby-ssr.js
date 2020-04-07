2cc37039d2012e5864ad398ae4221d0c915746ad
Ruben Costa <costamartinezruben@gmail.com>
2020-04-07 18:50:45 +0100

    add navbar-overlay onRenderBody

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,6 +4,7 @@ export const wrapRootElement = wrap
 
 const onRenderBody = ({ setPostBodyComponents }) => {
 	setPostBodyComponents([<div key="cookie-overlay" id="cookie-overlay" />])
+	setPostBodyComponents([<div key="navbar-overlay" id="navbar-overlay" />])
 }
 
 export { onRenderBody }
