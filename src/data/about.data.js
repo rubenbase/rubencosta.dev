8fe0e8ebe3e5bf0301b25628e770e5ff7b02e9eb
Ruben Costa <costamartinezruben@gmail.com>
2020-04-24 00:45:44 +0100

    complete experience mock data

--- src/data/about.data.js
+++ src/data/about.data.js
@@ -2,32 +2,121 @@ export const companiesData = [
 	{
 		id: 'vodafone',
 		title: 'Vodafone',
-		position: 'Software Engineer',
-		dateInit: '',
-		dateFinish: '',
-		location: 'London',
-		description: `Responsibilities: 
-      SCRUM, SASS, React, Typescript, Nodejs, Jest, Enzyme, Devops, AWS)`
+		positions: [
+			{
+				position: 'Software Developer',
+				dateInit: new Date(2019, 8),
+				dateFinish: null,
+				location: 'London, UK',
+				description: `Development of internal libraries and consumer sites.
+				
+Responsibilities: 
+- SCRUM, SASS, React, Typescript, Nodejs, Jest, Enzyme, Devops, AWS`
+			}
+		]
 	},
+
 	{
 		id: 'hackabos',
 		title: 'Hack a BOS',
-		position: 'Software Engineer',
-		dateInit: '',
-		dateFinish: '',
-		location: 'Galicia',
-		description: `Responsible for teaching the Javascript module at the Hack a BOSS Bootcamp(hack-a-bos.com) the first two editions(~40 students).
+		positions: [
+			{
+				position: 'Founder & Shareholder',
+				dateInit: new Date(2019, 0),
+				dateFinish: new Date(2019, 8),
+				location: 'Spain',
+				description: `Hack a BOSS was a project part of Blue Ocean Start.
+
+Was a bootcamp where professional programmers from different companies(usually 5 per edition) taught programming to our students at their/our company’s offices.
+
+We had partnerships with companies so they could hire our graduates.
+
+Accomplished:
+
+- 20+ Partners.
+- 100+ Students.
+- Expanded to multiple offices in multiple cities.
+
+Sold my shares to a private investor.`
+			},
+			{
+				position: 'Javascript Instructor',
+				dateInit: new Date(2019, 0),
+				dateFinish: new Date(2019, 6),
+				location: 'Spain',
+				description: `Responsible for teaching the Javascript module at the Hack a BOSS Bootcamp(hack-a-bos.com) the first two editions(~40 students).
 
 The classes were ran after the working day hours Mon-Fry + 8 hours Saturdays.
-    
+
 Partnered with IT companies to make it easier for them to find talented programmers that can be incorporated on their teams.
-    
+
 Responsibilities:
-- Communication skills. 
+- Communication skills.
 - Do workshops.
 - Javascript fundamentals, clean code, algorithms, asynchronous and functional programming.
 - Organize the content and coordinate the production.
 - Deep introduction to Git and Github/Gitlab.
 - Complete exploration of Visual Studio Code.`
+			}
+		]
+	},
+	{
+		id: 'blueoceanstart',
+		title: 'Blue Ocean Start',
+		positions: [
+			{
+				position: 'Founder & Shareholder',
+				dateInit: new Date(2018, 5),
+				dateFinish: new Date(2019, 8),
+				location: 'Spain',
+				description: `Blue Ocean Start was a tech company with products for multiple sectors.
+
+- Hospitality
+- Education
+- HHRR
+
+Our clients ranged from small businesses to companies traded at the IGBM.
+
+Sold my shares to a private investor.`
+			},
+			{
+				position: 'Software Developer',
+				dateInit: new Date(2018, 5),
+				dateFinish: new Date(2019, 6),
+				location: 'Spain',
+				description: `Development of internal and external web and mobile projects as well as management of the AWS infrastructure for a range of applications.
+
+Coordinate the development team applying the SCRUM methodology.
+
+Responsibilities:
+HTML, SASS, Typescript, React, Redux, React Native, GraphQL w/ Apollo Client, Nodejs, Postgres, Redis, REST, Jest, Enzyme, Sinon, Travis CI, Docker, Linux, D3js, Stripe, AWS, Scrum.`
+			}
+		]
+	},
+	{
+		id: 'domesting',
+		title: 'Domesting',
+		positions: [
+			{
+				position: 'Lead Mobile Developer',
+				dateInit: new Date(2017, 7),
+				dateFinish: new Date(2018, 4),
+				location: 'Spain',
+				description: `Coordinate mobile's department at Domesting as well as develop native mobile apps.
+
+Responsibilities:
+Ionic 3, Angular 5, Typescript, Meteorjs, Node.js, Mongodb, DigitalOcean, Bash scripting, Linux, Scrum.`
+			},
+			{
+				position: 'Software Developer',
+				dateInit: new Date(2017, 3),
+				dateFinish: new Date(2018, 4),
+				location: 'Spain',
+				description: `Development and maintenance of Domesting Connect and Domesting.com
+
+Responsibilities:
+HTML, LESS, Blazejs, Meteorjs, Nodejs, Mongodb, DigitalOcean, Linux, Scrum.`
+			}
+		]
 	}
 ]
