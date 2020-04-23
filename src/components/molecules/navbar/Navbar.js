720612c3ea052c38d11c43186d6e5564c5444cb8
Ruben Costa <costamartinezruben@gmail.com>
2020-04-24 00:43:55 +0100

    wip add navbar mobile

--- src/components/molecules/navbar/Navbar.js
+++ src/components/molecules/navbar/Navbar.js
@@ -1,63 +1,99 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import styled from '@emotion/styled'
-
+import Loadable from '@loadable/component'
+import { css } from '@emotion/core'
 import NavbarToggleButton from 'components/atoms/navbar-toggle-button'
-import NavbarDrawer from 'components/organisms/navbar-drawer'
+
 import { useNavbarIsOpenState, useNavbarIsOpenDispatch } from 'context/navbar-context'
+
+const NavbarDrawer = Loadable(() => import('components/organisms/navbar-drawer'))
 
 export default function Navbar() {
 	const { isOpen } = useNavbarIsOpenState()
-	const dispatch = useNavbarIsOpenDispatch()
+
 	return (
 		<>
-			{/*
-			<div>
-				 TODO: Finish navbar
-				<NavbarDrawer
-					closable={true}
-					visible={isOpen}
-					onClose={() => dispatch({ type: 'toggleIsOpen' })}
-				>
-					<h1>Hellooooo</h1>
-					<h1>Hellooooo</h1>
-					<h1>Hellooooo</h1>
-				</NavbarDrawer>
-				<NavbarToggleButton /> 
-			</div>
-				*/}
 			<Logo to="/">rubencosta</Logo>
 			<Nav>
 				<li>
-					<Link to="/blog">Blog</Link>
+					<StyledLink to="/blog">Blog</StyledLink>
 				</li>
 				<li>
-					<Link to="/about">About</Link>
+					<StyledLink to="/about">About</StyledLink>
 				</li>
 				<li>
-					<a href="https://github.com/rubenbase" target="_blank">
+					<a
+						css={theme => css`
+							color: ${theme.colors.dark100};
+						`}
+						href="https://github.com/rubenbase"
+						target="_blank"
+					>
 						Github
 					</a>
 				</li>
 			</Nav>
+
+			<NavbarDrawer isOpen={isOpen}>
+				<NavbarDrawerListItem>
+					<StyledLink to="/blog">Blog</StyledLink>
+				</NavbarDrawerListItem>
+				<NavbarDrawerListItem>
+					<StyledLink to="/about">About</StyledLink>
+				</NavbarDrawerListItem>
+				<NavbarDrawerListItem>
+					<a
+						css={theme => css`
+							color: ${theme.colors.dark100};
+							opacity: 0.5;
+							&:hover {
+								opacity: 1;
+							}
+						`}
+						href="https://github.com/rubenbase"
+						target="_blank"
+					>
+						Github
+					</a>
+				</NavbarDrawerListItem>
+			</NavbarDrawer>
+			<NavbarToggleButton />
 		</>
 	)
 }
 
-const Logo = styled(Link)`
+export const NavbarDrawerListItem = styled.li`
+	color: ${({ theme }) => theme.colors.dark100};
+	margin-bottom: 3em;
+	font-family: 'Roboto';
+	font-weight: 500;
+	font-size: 26px;
+`
+export const Logo = styled(Link)`
 	/* font-weight: bold; */
 	color: ${({ theme }) => theme.colors.dark100};
 	font-family: 'Roboto';
 	font-weight: 500;
 	font-size: 26px;
 `
+export const StyledLink = styled(Link)`
+	color: ${({ theme }) => theme.colors.dark100};
+	opacity: 0.5;
+	&:hover {
+		opacity: 1;
+	}
+`
 
 const Nav = styled.ul`
-	display: flex;
+	display: none;
 
-	li a {
-		color: ${({ theme }) => theme.colors.dark100};
-		font-size: 1.2em;
-		padding: 0 0.6em;
+	@media (min-width: 599px) {
+		display: flex;
+		li a {
+			color: ${({ theme }) => theme.colors.dark100};
+			font-size: 1.2em;
+			padding: 0 0.6em;
+		}
 	}
 `
